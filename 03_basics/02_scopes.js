ae9aea574b01cc492b44e8ea0ff38b197f67cd35
sahilpatel-2610 <kakadiyasahil102@gmail.com>
2025-06-10 03:52:27 +0000

    scope lev and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,14 +3,57 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-   console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+function one(){
+    const username = "naresh"
+
+    function tow(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    tow()
     
 }
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+// one()
+
+if (true) {
+    const username = "naresh"
+    if (username === "naresh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+(addTow(5))
+const addTow = function(num){
+    return num + 2
+}
+
